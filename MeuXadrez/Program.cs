ced062ef7ed8ad7a2a90db2ad58c35122a2ac8e2
Rafael Isac <rafaeldarrigo@gmail.com>
2024-04-15 13:38:14 -0300

    Classe Posicao Criada

--- MeuXadrez/Program.cs
+++ MeuXadrez/Program.cs
@@ -1,11 +1,18 @@
 ﻿using System;
+using tabuleiro;
 namespace MeuXadrez
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Posicao P;
 
+            P = new(3, 4);
+
+            Console.WriteLine("posi: "+P);
+
+            Console.ReadLine();
         }
     }
 }
